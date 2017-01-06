c8017f88f403dd5e2a38c5ab51a514187692ed4e
Melvil Adapoor <m0a00fe@m-C02RL7TCG8WN.local>
2017-01-06 20:30:39 +0530

    updated index

--- heap.py
+++ heap.py
@@ -3,44 +3,40 @@
 
 
 def max_heapify(arr, i, n):
-    if i > 0:
-        left = 2 * i
-        right = 2 * i + 1
-        if left <= n and arr[i - 1] < arr[left - 1]:
+        left = 2 * i +1
+        right = 2 * i + 2
+        if left <= n and arr[i] < arr[left]:
             largest = left
         else:
             largest = i
 
-        if right <= n and arr[largest - 1] < arr[right - 1]:
+        if right <= n and arr[largest] < arr[right]:
             largest = right
-        if arr[largest - 1] > arr[i - 1]:
-            (arr[i - 1], arr[largest - 1]) = (arr[largest - 1], arr[i
-                    - 1])
+        if arr[largest] > arr[i]:
+            (arr[i], arr[largest]) = (arr[largest], arr[i])
         if i != largest:
             max_heapify(arr, largest, n)
 
 
 def min_heapify(arr, i, n):
-    if i > 0:
-        left = 2 * i
-        right = 2 * i + 1
-        if left <= n and arr[i - 1] > arr[left - 1]:
+        left = 2 * i+1
+        right = 2 * i + 2
+        if left <= n and arr[i] > arr[left]:
             smallest = left
         else:
             smallest = i
 
-        if right <= n and arr[smallest - 1] > arr[right - 1]:
+        if right <= n and arr[smallest] > arr[right]:
             smallest = right
-        if arr[smallest - 1] < arr[i - 1]:
-            (arr[i - 1], arr[smallest - 1]) = (arr[smallest - 1], arr[i
-                    - 1])
+        if arr[smallest] < arr[i]:
+            (arr[i], arr[smallest]) = (arr[smallest], arr[i])
         if i != smallest:
             min_heapify(arr, smallest, n)
 
 arr=[7,4,6,1,9,8,3,5,2]
 i = len(arr) / 2
-while i > 0:
-    max_heapify(arr, i, 9)
+while i >= 0:
+    min_heapify(arr, i, 8)
     i -= 1
 
 print arr
